b2c7b9825ba744cb6b4ec95b633ed046130d8bfe
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-02-20 12:02:06 +0530

    Update practice_4.py

--- fundamentals/practice_4.py
+++ fundamentals/practice_4.py
@@ -60,5 +60,36 @@ print(is_divisible_by_five(12))  # False
 print(is_divisible_by_five(0))   # True
 print(is_divisible_by_five(-15)) # True
 
+# Factorial of a number using recursion
+def recur_factorial(n):
+   if n == 1:
+       return n
+   else:
+       return n*recur_factorial(n-1)
+num = 7
+# check if the number is negative
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   print("The factorial of", num, "is", recur_factorial(num))
+# The factorial of 7 is 5040
+
+# challenge:
+def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
+# Example usage:
+number = 5
+print(f"The factorial of {number} is {factorial(number)}.")
+
 
 
