9883bd8f654d636423b230977183320b2186eb0c
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-02-24 21:34:45 +0530

    Update 01 sep, end.py

--- Advance code/Python Test/01 sep, end.py
+++ Advance code/Python Test/01 sep, end.py
@@ -38,7 +38,69 @@ complex_number = 2 + 3j
 print(complex_number.real)
 print(complex_number.imag)
 
+b = abs(-10) # Return the absolute value of the argument.
+print(b)
+c =abs(-9)
+print(c)
+# 10
+# 9
 
+name = "This is a Big line"
+print(type(name))
+name = name + str(1)  # can only concatenate str (not "int") to str
+print(name)
+
+first_name = "Pramod"
+last_name = "Dutta"
+full_name = first_name + " " + last_name
+print(full_name)
+print(type(full_name))
+
+a = "ankit"
+b = "kumar"
+c = a + " "+ b
+print(c)
+
+dir = r'C:\pramod\n.txt'
+print(dir)
+
+dir = r"C:\pramod\t.txt"
+print(dir)
+
+dir = "C:\pramod\t.txt"
+print(dir)
+
+print(5 % 2) # Modules
+print(5 // 2)
+
+# 2 | 5 | 2 - Quotient
+#   | 4 |
+# --------
+#     1 - Remainder
+
+print(13 // 2)
+print(13 % 2)
+
+q, r = divmod(5, 2)
+print(q)
+print(r)
+
+radius = 2
+import math
+print(math.pi)
+print(math.pow(radius,2))
+area = math.pi * math.pow(radius, 2)
+print("Area of the circle is -> ", area)
+
+def greet():
+    print("Hello, Welcome to Python World!")
+    
+greet()
+greet()
+greet()
+
+print("dasdadads")
+greet()
 
 
 
