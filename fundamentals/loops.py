f8f136ef174712b97ddaadaf494f975e229a1e10
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-10 00:18:31 +0530

    Update loops.py

--- fundamentals/loops.py
+++ fundamentals/loops.py
@@ -1,3 +1,10 @@
+terms = int(input("How many terms? "))
+# use anonymous function
+result = list(map(lambda x: 2 ** x, range(terms)))
+print("The total terms are:",terms)
+for i in range(terms):
+   print("2 raised to power",i,"is",result[i])
+
 #challenge
 def fibonacci_less_than(n):
     # Start with the first two Fibonacci numbers
