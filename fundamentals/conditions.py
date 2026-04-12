a950c7e8239444cdcfcd087cca37a9ce69e75c29
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-12 20:03:04 +0530

    Update conditions.py

--- fundamentals/conditions.py
+++ fundamentals/conditions.py
@@ -1,3 +1,18 @@
+# challenge:
+def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
+# Example usage:
+number = 5
+print(f"The factorial of {number} is {factorial(number)}.")
+
 # Factorial of a number using recursion
 def recur_factorial(n):
    if n == 1:
